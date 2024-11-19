fd62ed7dd113938777d33769def5d62184daf2c6
Jesse Kretschmer <jesse@krets.com>
2024-11-19 23:54:53 +0100

    Caching for offline play

---
+++ krets/blocks/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'blocks-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/blocks/',
+  '/blocks/blocks.js',
+  '/blocks/blocks.css',
+  '/blocks/favicon.ico',
+  '/blocks/blocks-192x192.png',
+  '/blocks/blocks-512x513.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
